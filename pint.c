fafcaed8d0133cbf9633171722d926f6b6a31b8e
Sonlowami <uwilowami64@outlook.com>
2022-10-05 18:59:52 +0200

    Print error

--- pint.c
+++ pint.c
@@ -2,10 +2,15 @@
 /**
  * pint - print an item at the top of the stack
  * @stack: pointer to the reference to the TOS
- * @line_number: unused here
+ * @line_number: to print in an error message
  */
 void pint(stack_t **stack, __attribute__((unused))unsigned int line_number)
 {
 	if (stack && *stack)
 		printf("%d\n", (*stack)->n);
+	else
+	{
+		fprintf(stderr, "L%u: can't pint,stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 }
