66087493f33cefb6269ebc33040fdc812cff8e6d
Sonlowami <uwilowami64@outlook.com>
2022-10-06 23:46:34 +0200

    Add a newline after fprintf functions

--- ptchar.c
+++ ptchar.c
@@ -8,12 +8,12 @@ void ptchar(stack_t **stack, unsigned int line_number)
 {
 	if (!stack || !*stack)
 	{
-		fprintf(stderr, "L%u: can't ptchar, stack empty", line_number);
+		fprintf(stderr, "L%u: can't ptchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	if ((*stack)->n > 127 || (*stack)->n < 0)
 	{
-		fprintf(stderr, "L%u: can't ptchar, value out of range", line_number);
+		fprintf(stderr, "L%u: can't ptchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%c\n", (*stack)->n);
